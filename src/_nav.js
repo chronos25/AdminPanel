d2a671c81b3c4b4c6eb00269344d0bec7c8fc76d
Ritik Raj <33518732+chronos25@users.noreply.github.com>
2020-02-21 11:32:35 +0530

    Update _nav.js

--- src/_nav.js
+++ src/_nav.js
@@ -10,145 +10,35 @@ export default {
       },
     },
     {
-      name: 'Mentors',
+      name: 'Get Mentors',
       url: '/base/cards',
       icon: 'icon-puzzle'
     },
     {
-      name: "Create Mentor's",
+      name: 'Get Mentor By Id',
+      url: '/base/cards',
+      icon: 'icon-puzzle'
+    },
+    {
+      name: "Add New Mentor",
       url: '/base/forms',
       icon: 'icon-pie-chart',
     },
     {
-      name: "Update Mentor's",
+      name: "Update Mentor By Id",
       url: '/base/forms',
+      icon: 'icon-puzzle'
+    },
+    {
+      name: "Delete Mentor By Id",
+      url: '/base/Jumbotrons',
       icon: 'icon-puzzle'
     },
     {
       name: "Delete All Mentor's",
-      url: '/base/forms',
+      url: '/base/Jumbotrons',
       icon: 'icon-puzzle'
-    },
-    {
-      name: 'Icons',
-      url: '/icons',
-      icon: 'icon-star',
-      children: [
-        {
-          name: 'CoreUI Icons',
-          url: '/icons/coreui-icons',
-          icon: 'icon-star',
-          badge: {
-            variant: 'info',
-            text: 'NEW',
-          },
-        },
-        {
-          name: 'Flags',
-          url: '/icons/flags',
-          icon: 'icon-star',
-        },
-        {
-          name: 'Font Awesome',
-          url: '/icons/font-awesome',
-          icon: 'icon-star',
-          badge: {
-            variant: 'secondary',
-            text: '4.7',
-          },
-        },
-        {
-          name: 'Simple Line Icons',
-          url: '/icons/simple-line-icons',
-          icon: 'icon-star',
-        },
-      ],
-    },
-    {
-      name: 'Notifications',
-      url: '/notifications',
-      icon: 'icon-bell',
-      children: [
-        {
-          name: 'Alerts',
-          url: '/notifications/alerts',
-          icon: 'icon-bell',
-        },
-        {
-          name: 'Badges',
-          url: '/notifications/badges',
-          icon: 'icon-bell',
-        },
-        {
-          name: 'Modals',
-          url: '/notifications/modals',
-          icon: 'icon-bell',
-        },
-      ],
-    },
-    {
-      name: 'Widgets',
-      url: '/widgets',
-      icon: 'icon-calculator',
-      badge: {
-        variant: 'info',
-        text: 'NEW',
-      },
-    },
-    {
-      divider: true,
-    },
-    {
-      title: true,
-      name: 'Extras',
-    },
-    {
-      name: 'Pages',
-      url: '/pages',
-      icon: 'icon-star',
-      children: [
-        {
-          name: 'Login',
-          url: '/login',
-          icon: 'icon-star',
-        },
-        {
-          name: 'Register',
-          url: '/register',
-          icon: 'icon-star',
-        },
-        {
-          name: 'Error 404',
-          url: '/404',
-          icon: 'icon-star',
-        },
-        {
-          name: 'Error 500',
-          url: '/500',
-          icon: 'icon-star',
-        },
-      ],
-    },
-    {
-      name: 'Disabled',
-      url: '/dashboard',
-      icon: 'icon-ban',
-      attributes: { disabled: true },
-    },
-    {
-      name: 'Download CoreUI',
-      url: 'https://coreui.io/react/',
-      icon: 'icon-cloud-download',
-      class: 'mt-auto',
-      variant: 'success',
-      attributes: { target: '_blank', rel: "noopener" },
-    },
-    {
-      name: 'Try CoreUI PRO',
-      url: 'https://coreui.io/pro/react/',
-      icon: 'icon-layers',
-      variant: 'danger',
-      attributes: { target: '_blank', rel: "noopener" },
-    },
+    }
+    
   ],
 };
